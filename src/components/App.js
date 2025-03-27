14ce586d07c4c3e3acfa6e31ec3210dd09b2a031
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-03-27 16:27:00 +0530

    completed...

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,64 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false,
+      posi: 0,
+      ballPosition: {
+        left: "0px",
+        position: "absolute", // CSS positioning ke liye important
+      },
     };
+    this.renderBallOrButton = this.renderBallOrButton.bind(this);
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+  }
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
-    }
+  // Handles start button click, rendering the ball
+  buttonClickHandler() {
+    this.setState({
+      renderBall: true,
+    });
+  }
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
+  // Determines whether to show start button or ball
+  renderBallOrButton() {
+    if (this.state.renderBall) {
+      return <div className="ball" style={this.state.ballPosition}></div>;
+    } else {
+      return (
+        <button className="start" onClick={this.buttonClickHandler}>
+          Start
+        </button>
+      );
     }
+  }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
-    }
+  // bind ArrowRight keydown event
+  componentDidMount() {
+    document.addEventListener("keydown", (event) => {
+      // Check for Right Arrow key (keyCode: 39) and game started
+      if (event.keyCode === 39 && this.state.renderBall) {
+        this.setState((prevState) => ({
+          posi: prevState.posi + 5,
+          ballPosition: {
+            ...prevState.ballPosition,
+            left: `${prevState.posi + 5}px`,
+          },
+        }));
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  render() {
+    return <div className="playground">{this.renderBallOrButton()}</div>;
+  }
 }
-
 
 export default App;
